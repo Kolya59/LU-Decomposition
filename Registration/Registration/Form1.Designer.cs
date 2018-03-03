dc69af03880a4d60d211c2b207d7baeb88791ad9
Kolya59 <kolya.nikitas@yandex.ru>
2018-03-03 18:19:18 +0500

    Initial commit

---
+++ Registration/Registration/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btRegister = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbhashedPass = new System.Windows.Forms.TextBox();
+            this.bt_signin = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btRegister
+            // 
+            this.btRegister.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btRegister.Location = new System.Drawing.Point(12, 147);
+            this.btRegister.Name = "btRegister";
+            this.btRegister.Size = new System.Drawing.Size(141, 30);
+            this.btRegister.TabIndex = 1;
+            this.btRegister.Text = "Регистрация";
+            this.btRegister.UseVisualStyleBackColor = true;
+            this.btRegister.Click += new System.EventHandler(this.BtRegister_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbLogin);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 56);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Логин";
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(8, 19);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(310, 20);
+            this.tbLogin.TabIndex = 0;
+            this.tbLogin.TextChanged += new System.EventHandler(this.TbLogin_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbhashedPass);
+            this.groupBox2.Location = new System.Drawing.Point(12, 71);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(324, 54);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Пароль";
+            // 
+            // tbhashedPass
+            // 
+            this.tbhashedPass.Location = new System.Drawing.Point(8, 19);
+            this.tbhashedPass.Name = "tbhashedPass";
+            this.tbhashedPass.Size = new System.Drawing.Size(310, 20);
+            this.tbhashedPass.TabIndex = 1;
+            // 
+            // bt_signin
+            // 
+            this.bt_signin.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bt_signin.Location = new System.Drawing.Point(201, 146);
+            this.bt_signin.Name = "bt_signin";
+            this.bt_signin.Size = new System.Drawing.Size(129, 30);
+            this.bt_signin.TabIndex = 6;
+            this.bt_signin.Text = "Вход";
+            this.bt_signin.UseVisualStyleBackColor = true;
+            this.bt_signin.Click += new System.EventHandler(this.Bt_signin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 196);
+            this.Controls.Add(this.bt_signin);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btRegister);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Регистрация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btRegister;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbhashedPass;
+        private System.Windows.Forms.Button bt_signin;
+    }
+}
+
